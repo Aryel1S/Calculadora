bddc411776cc7048c0edbef82d0b419731eccc13
Aryel1S <aryelo82@gmail.com>
2021-11-24 23:32:18 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+class Calculator {
+    constructor(previousOperandTextElement,currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+}
+
+
 const numberButtons = document.querySelectorAll ('[data-number]')
 const operationButtons = document.querySelectorAll ('[data-operation]')
 const equalsButton = document.querySelectorAll ('[data-equals]')
-const deleteButton = document.querySelectorAll ('[data-equals]')
-const allClearButton = document.querySelectorAll ('[data-equals]')
-const equalsButton = document.querySelectorAll ('[data-equals]')
+const deleteButton = document.querySelectorAll ('[data-delete]')
+const allClearButton = document.querySelectorAll ('[data-all-clear]')
+const previousOperandTextElement = document.querySelectorAll ('[data-previous-operand]')
+const currentOperandTextElement = document.querySelectorAll ('[data-current-operand]')
+
