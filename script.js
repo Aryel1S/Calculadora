b882fedb49b21550df59bfb97afc86b666ea7323
Aryel1S <aryelo82@gmail.com>
2021-11-24 23:30:37 -0300

    atulizacao

---
+++ script.js
@@ -0,0 +1,6 @@
+const numberButtons = document.querySelectorAll ('[data-number]')
+const operationButtons = document.querySelectorAll ('[data-operation]')
+const equalsButton = document.querySelectorAll ('[data-equals]')
+const deleteButton = document.querySelectorAll ('[data-equals]')
+const allClearButton = document.querySelectorAll ('[data-equals]')
+const equalsButton = document.querySelectorAll ('[data-equals]')
